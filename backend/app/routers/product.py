225888a6522b6385ee51f25f27b7ac87c49ec4e5
parallax <narakagaara@gmail.com>
2025-07-17 17:13:37 -0300

    Changed name to product.py

--- backend/app/routers/product.py
+++ backend/app/routers/product.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 from typing import List, Optional
 from app.schemas.product import Product, ProductTypeEnum
-from app.models.product import ProductModel
 from app.db.database_connection import AsyncSessionLocal
+from app.services.product_service import get_all_products_service, get_product_by_id_service
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
@@ -18,19 +17,8 @@ async def get_all_products(
     type: Optional[ProductTypeEnum] = Query(None),
     name: Optional[str] = Query(None)
 ):
-    query = select(ProductModel)
-    if type:
-        query = query.where(ProductModel.type == type)
-    if name:
-        query = query.where(ProductModel.name.ilike(f"%{name}%"))
-    result = await db.execute(query)
-    products = result.scalars().all()
-    return products
+    return await get_all_products_service(db=db, type=type, name=name)
 
 @router.get("/{product_id}", response_model=Product)
 async def get_product_by_id(product_id: int, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(ProductModel).where(ProductModel.id == product_id))
-    product = result.scalar_one_or_none()
-    if not product:
-        raise HTTPException(status_code=404, detail="Produto não encontrado.")
-    return product
+    return await get_product_by_id_service(product_id=product_id, db=db)
