c56252f624a973471d17772117ccc2a8bfc0604b
parallax <narakagaara@gmail.com>
2025-07-18 18:47:03 -0300

    Added authenticate_user method

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -2,10 +2,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.models.user import User
 from app.schemas.user import UserCreate
-from app.core.security import get_password_hash
+from app.core.security import get_password_hash, verify_password
 
 async def create_user(db: AsyncSession, user_in: UserCreate) -> User:
-    password= get_password_hash(user_in.password)
+    password = get_password_hash(user_in.password)
     db_user = User(email=user_in.email, password=password)
     db.add(db_user)
     await db.commit()
@@ -15,3 +15,11 @@ async def create_user(db: AsyncSession, user_in: UserCreate) -> User:
 async def get_user_by_email(db: AsyncSession, email: str) -> User | None:
     result = await db.execute(select(User).where(User.email == email))
     return result.scalars().first()
+
+async def authenticate_user(db: AsyncSession, email: str, password: str) -> User | None:
+    user = await get_user_by_email(db, email)
+    if not user:
+        return None
+    if not verify_password(password, user.password):
+        return None
+    return user
