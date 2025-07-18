a6c0d42f9763fd721978a6b1d0a9a8efb9c9f315
parallax <narakagaara@gmail.com>
2025-07-18 18:45:46 -0300

    Added verify_password and create_access_token methods

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,6 +1,26 @@
+import jwt
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
+from app.core.config import settings
+from datetime import datetime, timedelta, timezone
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+def verify_password(password: str, hashed_password) -> bool:
+    return pwd_context.verify(password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    return encoded_jwt
