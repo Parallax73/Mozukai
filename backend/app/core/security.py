808cd57ed91f0e3df51ba185308cdccf56cb8269
parallax <narakagaara@gmail.com>
2025-07-20 15:01:19 -0300

    Refactored create_token to reuse with multiple expire time

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -15,12 +15,21 @@ def verify_password(password: str, hashed_password) -> bool:
     return pwd_context.verify(password, hashed_password)
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_token(data: dict, minutes: int) -> str:
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=minutes)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
     return encoded_jwt
+
+
+def create_access_token(data: dict) -> str:
+    return create_token(data, settings.access_token_lifetime)
+
+
+def create_refresh_token(data: dict, remember_me: bool = False) -> str:
+    if remember_me:
+        minutes = settings.long_refresh_token_lifetime
+    else:
+        minutes = settings.short_refresh_token_lifetime
+    return create_token(data, minutes)
