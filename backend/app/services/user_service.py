0e0546f6f1f39d3c3481a8abe2907fb017d8892f
parallax <narakagaara@gmail.com>
2025-07-20 15:02:17 -0300

    Added email already exists validation

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,5 +1,7 @@
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy.exc import IntegrityError
 from app.models.user import User
 from app.schemas.user import UserCreate
 from app.core.security import get_password_hash, verify_password
@@ -8,7 +10,14 @@ async def create_user(db: AsyncSession, user_in: UserCreate) -> User:
     password = get_password_hash(user_in.password)
     db_user = User(email=user_in.email, password=password)
     db.add(db_user)
-    await db.commit()
+    try:
+        await db.commit()
+    except IntegrityError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="This email is already in use."
+        )
     await db.refresh(db_user)
     return db_user
 
