f33c331977d56b86a399ac592903e14eb28fa920
parallax <narakagaara@gmail.com>
2025-07-20 22:13:29 -0300

    Refactor register and added withCredential to loginUser request

--- frontend/src/services/UserService.ts
+++ frontend/src/services/UserService.ts
@@ -3,21 +3,14 @@ import axios from "axios";
 const UserService = {
   baseUrl: 'http://localhost:8000',
 
-  async registerUser(email: string, password: string){
-      const url = new URL(this.baseUrl + '/register');
-      const data = {
-        'email' : email,
-        'password' : password
-      };
+  async registerUser(email: string, password: string) {
+    const url = new URL(this.baseUrl + '/register');
+    const data = {
+      email,
+      password
+    };
 
-      try {
-        await axios.post(url.toString(), data);
-      } catch (err) {
-      if (err instanceof Error) {
-        throw err;
-      }
-      throw new Error('Unknown error');
-    }
+    await axios.post(url.toString(), data);
   },
 
   async loginUser(email: string, password: string): Promise<string> {
@@ -26,35 +19,30 @@ const UserService = {
     formData.append('username', email);
     formData.append('password', password);
 
-    try {
-      const response = await axios.post(url.toString(), formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        withCredentials: true, 
-      });
+    const response = await axios.post(url.toString(), formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      withCredentials: true
+    });
 
-      return response.data.access_token;
-    } catch (err) {
-      if (err instanceof Error) {
-        throw err;
-      }
-      throw new Error('Unknown error');
-    }
+    return response.data.access_token;
+  },
+
+  async refreshAccessToken(): Promise<string> {
+    const url = new URL(this.baseUrl + '/refresh');
+    const response = await axios.post(url.toString(), null, {
+      withCredentials: true,
+    });
+
+    return response.data.access_token;
   },
 
   async logoutUser() {
     const url = new URL(this.baseUrl + '/logout');
-    try {
-      await axios.post(url.toString(), null, {
-        withCredentials: true,
-      });
-    } catch (err) {
-      if (err instanceof Error) {
-        throw err;
-      }
-      throw new Error('Unknown error');
-    }
+    await axios.post(url.toString(), null, {
+      withCredentials: true,
+    });
   },
 };
 
