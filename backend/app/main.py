454759280165d6df615202220180157d1d836a8c
parallax <narakagaara@gmail.com>
2025-07-23 21:00:34 -0300

    Added payment router to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import product, user
+from app.routers import product, user , payments
 from app.db.database_connection import engine, Base
 from app.core.config import settings
 import logging
@@ -25,3 +25,4 @@ async def startup_event():
 
 app.include_router(product.router)
 app.include_router(user.router)
+app.include_router(payments.router)
