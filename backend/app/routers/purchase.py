096cf1c63090da74a481572e8372ee54edf4360a
parallax <narakagaara@gmail.com>
2025-08-03 14:33:45 -0300

    add: added get methods to retrieve purchases

--- backend/app/routers/purchase.py
+++ backend/app/routers/purchase.py
@@ -1,10 +1,10 @@
-from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status, Request, Response, Cookie
+from typing import List, Literal, Optional
+from fastapi import APIRouter, Depends, HTTPException, Query, status, Request, Response, Cookie
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.dependencies import get_db
 from app.schemas.purchase import PurchaseCreate
-from app.services.puchase_service import create_purchase, get_purchase_by_id, get_purchase_by_product_id, get_purchases
-from app.models.purchase import PurchaseModel
+from app.services.puchase_service import create_purchase, get_purchase_by_id, get_purchase_by_product_id, get_filtered_purchases
+from app.models.purchase import PurchaseModel, StatusTypeEnum
 import logging
 
 
@@ -27,8 +27,59 @@ async def create(purchase: PurchaseCreate, db: AsyncSession = Depends(get_db)):
         The created purchase object.
     """
     logger.info(f"Attempting to create purchase for user {purchase.email}")
-    product = await create_purchase(db=db, purchase=purchase)
-    return product
+    purchase = await create_purchase(db=db, purchase=purchase)
+    return purchase
+
+
+@router.get("/get-purchases", response_model=List[PurchaseModel])
+async def get(
+    status: Optional[StatusTypeEnum] = Query(None),
+    sort_date: Optional[Literal["asc", "desc"]] = Query("desc"),
+    skip: int = Query(0, ge=0),
+    limit: int = Query(100, ge=1),
+    db: AsyncSession = Depends(get_db)
+) -> List[PurchaseModel]:
+    """
+    Returns:
+        A filtered list of purchases, or all if no filters are applied
+    """
+    logger.info("Fetching a list of purchases")
+    purchases = await get_filtered_purchases(
+        db=db,
+        status=status,
+        sort_date=sort_date,
+        skip=skip,
+        limit=limit
+    )
+    return purchases
+
+
+@router.get("/get-purchase/{purchase_id}")
+async def get_by_id(purchase_id: int, db: AsyncSession = Depends(get_db)) -> PurchaseModel:
+    """
+    Args:
+        purchade_id: Id from the purchase the user wants to retrieve
+
+    Returns:
+        The purchase with the id the user entered
+    """
+    logger.info(f"Trying to fetch a purchase by the id: {purchase_id}")
+    purchase = await get_purchase_by_id(purchase_id=purchase_id,db=db)
+    return purchase
+
+
+@router.get("/get-purchase-by-product/{product_id}")
+async def get_by_product_id(product_id: int, db: AsyncSession = Depends(get_db)) -> PurchaseModel:
+    """
+    Args:
+        purchade_id: Id from the purchase the user wants to retrieve
+
+    Returns:
+        The purchase with the id the user entered
+    """
+    logger.info(f"Trying to fetch a purchase by the id: {product_id}")
+    purchase = await get_purchase_by_product_id(product_id=product_id,db=db)
+    return purchase
 
 
 
