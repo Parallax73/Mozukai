e0b9254e7692b9540080c2e014084cb88346fd2c
parallax <narakagaara@gmail.com>
2025-08-03 14:35:41 -0300

    add: created get_filtered_purchases to retrieve purchases with parameters

--- backend/app/services/puchase_service.py
+++ backend/app/services/puchase_service.py
@@ -1,13 +1,15 @@
 import logging
-from typing import List
+from typing import List, Optional, Literal
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from fastapi import HTTPException, status
+from sqlalchemy import select, desc, asc
+from fastapi import HTTPException, status as st
 from app.models.purchase import PurchaseModel
 from app.schemas.purchase import PurchaseCreate
+from app.schemas.purchase import StatusTypeEnum
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
 
 async def create_purchase(db: AsyncSession, purchase: PurchaseCreate) -> PurchaseModel:
     try:
@@ -20,7 +22,8 @@ async def create_purchase(db: AsyncSession, purchase: PurchaseCreate) -> Purchas
             city=purchase.city,
             state=purchase.state,
             cep=purchase.cep,
-            status=purchase.status
+            status=purchase.status,
+            date=purchase.date
         )
         db.add(db_purchase)
         await db.commit()
@@ -31,9 +34,10 @@ async def create_purchase(db: AsyncSession, purchase: PurchaseCreate) -> Purchas
         await db.rollback()
         logger.error(f"Error creating purchase: {str(e)}")
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code=st.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Failed to create purchase"
         )
+
 
 async def get_purchases(db: AsyncSession, skip: int = 0, limit: int = 100) -> List[PurchaseModel]:
     try:
@@ -44,9 +48,41 @@ async def get_purchases(db: AsyncSession, skip: int = 0, limit: int = 100) -> Li
     except Exception as e:
         logger.error(f"Error retrieving purchases: {str(e)}")
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code=st.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Failed to retrieve purchases"
         )
+
+
+async def get_filtered_purchases(
+    db: AsyncSession,
+    status: Optional[StatusTypeEnum] = None,
+    sort_date: Optional[Literal["asc", "desc"]] = "desc",
+    skip: int = 0,
+    limit: int = 100
+) -> List[PurchaseModel]:
+    try:
+        query = select(PurchaseModel)
+
+        if status:
+            query = query.where(PurchaseModel.status == status)
+
+        if sort_date == "asc":
+            query = query.order_by(asc(PurchaseModel.date))
+        else:
+            query = query.order_by(desc(PurchaseModel.date))
+
+        query = query.offset(skip).limit(limit)
+        result = await db.execute(query)
+        purchases = result.scalars().all()
+        logger.info(f"Retrieved {len(purchases)} filtered purchases")
+        return list(purchases)
+    except Exception as e:
+        logger.error(f"Error retrieving filtered purchases: {str(e)}")
+        raise HTTPException(
+            status_code=st.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to retrieve filtered purchases"
+        )
+
 
 async def get_purchase_by_id(db: AsyncSession, purchase_id: int) -> PurchaseModel:
     try:
@@ -55,7 +91,7 @@ async def get_purchase_by_id(db: AsyncSession, purchase_id: int) -> PurchaseMode
         if not purchase:
             logger.warning(f"Purchase with ID {purchase_id} not found")
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
+                status_code=st.HTTP_404_NOT_FOUND,
                 detail="Purchase not found"
             )
         logger.info(f"Retrieved purchase with ID {purchase_id}")
@@ -63,19 +99,26 @@ async def get_purchase_by_id(db: AsyncSession, purchase_id: int) -> PurchaseMode
     except Exception as e:
         logger.error(f"Error retrieving purchase by ID: {str(e)}")
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code=st.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Failed to retrieve purchase by ID"
         )
 
-async def get_purchase_by_product_id(db: AsyncSession, product_id: int) -> List[PurchaseModel]:
+async def get_purchase_by_product_id(db: AsyncSession, product_id: int) -> PurchaseModel:
     try:
         result = await db.execute(select(PurchaseModel).where(PurchaseModel.product_id == product_id))
-        purchases = result.scalars().all()
-        logger.info(f"Retrieved {len(purchases)} purchases for product ID {product_id}")
-        return list(purchases)
+        purchase = result.scalar_one_or_none()
+        logger.info(f"Retrieved purchase for product ID {product_id}: {purchase}")
+        
+        if purchase is None:
+            raise HTTPException(
+                status_code=st.HTTP_404_NOT_FOUND,
+                detail=f"No purchase found for product {product_id}"
+            )
+            
+        return purchase
     except Exception as e:
-        logger.error(f"Error retrieving purchases for product {product_id}: {str(e)}")
+        logger.error(f"Error retrieving purchase for product {product_id}: {str(e)}")
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Failed to retrieve purchases for product {product_id}"
+            status_code=st.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to retrieve purchase for product {product_id}"
         )
