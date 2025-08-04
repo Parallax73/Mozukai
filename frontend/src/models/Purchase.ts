c11449e0e75b702acf68ee5bab86d7bae96b6da7
parallax <narakagaara@gmail.com>
2025-08-04 19:55:18 -0300

    add: created purchase model

--- frontend/src/models/Purchase.ts
+++ frontend/src/models/Purchase.ts
@@ -1,3 +1,39 @@
 export class Purchase {
-    
+    id: number;
+    product_id: number;
+    email: string;
+    name: string;
+    address: string;
+    complement: string;
+    city: string;
+    state: string;
+    cep: number;
+    status: 'pending' | 'paid' | 'processing' | "shipped" | 'delivered' | 'canceled' | 'refunded' | 'failed';
+    date: Date;
+
+    constructor(
+        id: number,
+        product_id: number,
+        email: string,
+        name: string,
+        address: string,
+        complement: string,
+        city: string,
+        state: string,
+        cep: number,
+        status: 'pending' | 'paid' | 'processing' | "shipped" | 'delivered' | 'canceled' | 'refunded' | 'failed',
+        date: Date
+    ) {
+        this.id = id;
+        this.product_id = product_id;
+        this.email = email;
+        this.name = name;
+        this.address = address;
+        this.complement = complement;
+        this.city = city;
+        this.state = state;
+        this.cep = cep;
+        this.status = status;
+        this.date = date;
+    }
 }
